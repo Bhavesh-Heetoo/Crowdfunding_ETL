20bac713c04e952bb49268bbc4496480933af4f9
Bhavesh-Heetoo <BhaveshH1234@gmail.com>
2024-11-28 20:18:37 -0500

    Adding updated schema
    Adding updated schema

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(200)   NOT NULL,
+    "description" varchar(200)   NOT NULL,
+    "goal" decimal   NOT NULL,
+    "pledged" decimal   NOT NULL,
+    "outcome" varchar(200)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(2)   NOT NULL,
+    "currency" varchar(3)   NOT NULL,
+    "launched_date" varchar(200)   NOT NULL,
+    "end_date" varchar(200)   NOT NULL,
+    "category_id" varchar(4)   NOT NULL,
+    "subcategory_id" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar   NOT NULL,
+    "category" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    -- PK int  FK >- dm.EmpNo
+    "subcategory_id" varchar(200)   NOT NULL,
+    "subcategory" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    -- PK int FK >- Employees.EmpNo FK >- d.DeptNo
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(200)   NOT NULL,
+    "last_name" varchar(200)   NOT NULL,
+    "email" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
